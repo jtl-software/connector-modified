5fe148be7fb4873f975f32f0143dd9acf217fea1
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2014-07-25 14:41:03 +0200

    initial commit

---
+++ src/jtl/Connector/Modified/Config/Loader/Config.php
@@ -0,0 +1,92 @@
+<?php
+namespace jtl\Connector\Modified\Config\Loader;
+
+use \jtl\Core\Config\Loader\Base as BaseLoader;
+use \jtl\Core\Config\Base as BaseConfig;
+use \jtl\Core\Filesystem\Tool;
+use \jtl\Core\Exception\ConfigException;
+
+class Config extends BaseLoader
+{
+
+    const DELIMITER = '::';
+
+    //Configuration params in modified-Config
+    const CFG_SHOP_PATH = 'DIR_FS_DOCUMENT_ROOT';
+    const CFG_SHOP_SERVER = 'HTTP_SERVER';
+    const CFG_SHOP_FOLDER = 'DIR_WS_CATALOG';
+    const CFG_DB_HOST = 'DB_SERVER';
+    const CFG_DB_NAME = 'DB_DATABASE';
+    const CFG_DB_USER = 'DB_SERVER_USERNAME';
+    const CFG_DB_PASS = 'DB_SERVER_PASSWORD';
+    const CFG_IMG_ORIGINAL = 'DIR_WS_ORIGINAL_IMAGES';
+
+    //Constant translations/keys
+    protected $trans = array(
+      self::CFG_SHOP_PATH => 'shop::path',
+      self::CFG_SHOP_SERVER => 'shop::url',
+      self::CFG_SHOP_FOLDER => 'shop::folder',
+      self::CFG_DB_HOST => 'db::host',
+      self::CFG_DB_NAME => 'db::name',
+      self::CFG_DB_USER => 'db::user',
+      self::CFG_DB_PASS => 'db::pass',
+      self::CFG_IMG_ORIGINAL => 'img::original'
+    );
+
+    protected $config_file;
+
+    protected $name = 'ModifiedConfig';
+
+    public function __construct($config_file)
+    {
+        $this->config_file = $config_file;
+    }
+
+    public function beforeRead()
+    {
+        if (!Tool::is_file($this->config_file)) {
+            throw new ConfigException(sprintf('Unable to load modified configuration file "%s"', $this->config_file), 100);
+        }
+        require_once $this->config_file;
+        $keys = $this->getConfigKeys();
+        $data = array();
+        if (!empty($keys)) {
+            foreach ($keys as $key => $value) {
+                $s = null;
+                if (defined($value)) {
+                    $s = constant($value);
+                }
+                if (strpos($this->trans[$value], self::DELIMITER) !== false) { // Special Key
+                    $ret = explode(self::DELIMITER, $this->trans[$value], 2);
+                    if (!empty($ret) && is_array($ret)) {
+                        if (!isset($data[$ret[0]])) {
+                            $data[$ret[0]] = array();
+                        }
+                        $data[$ret[0]][$ret[1]] = $s;
+                    }
+                }
+                else { // Default Key
+                    $data[$this->trans[$value]] = $s;
+                }
+            }
+            if (array_key_exists($this->trans[self::CFG_SHOP_PATH], $data)) {
+                $data[$this->trans[self::CFG_SHOP_PATH]] = realpath($data[$this->trans[self::CFG_SHOP_PATH]]);
+            }
+        }
+        $this->data = $data;
+    }
+
+    public function getConfigKeys()
+    {
+        $ecrc = new \ReflectionClass(get_called_class());
+        $consts = $ecrc->getConstants();
+        $keys = array();
+        foreach ($consts as $key => $value) {
+            if (substr($key, 0, 3) === 'CFG') {
+                $keys[$key] = $value;
+            }
+        }
+        return $keys;
+    }
+
+}
---
+++ src/jtl/Connector/Modified/Connector.php
@@ -0,0 +1,144 @@
+<?php
+namespace jtl\Connector\Modified;
+
+use \jtl\Core\Exception\TransactionException;
+use \jtl\Core\Exception\DatabaseException;
+use \jtl\Core\Rpc\RequestPacket;
+use \jtl\Core\Utilities\RpcMethod;
+use \jtl\Core\Controller\Controller as CoreController;
+use \jtl\Core\Database\Mysql;
+use \jtl\Connector\ModelContainer\MainContainer;
+use \jtl\Core\Rpc\ResponsePacket;
+use \jtl\Connector\Transaction\Handler as TransactionHandler;
+use \jtl\Connector\Session\SessionHelper;
+use \jtl\Connector\Base\Connector as BaseConnector;
+
+use \jtl\Connector\Modified\Config\Loader\Config as ConfigLoader;
+use jtl\Core\Rpc\Error as Error;
+
+class Connector extends BaseConnector
+{
+    protected $_controller;
+    protected $_action;
+	
+    protected function init()
+    {
+        $session = new SessionHelper("modified");
+		
+        register_shutdown_function(array($this,'shutdown_handler'));
+        set_error_handler(array($this,'error_handler'), E_ALL);
+        
+        $config = $this->getConfig();
+        $session->config = $config;
+        
+        // read modified configuration
+        if (!$config->existsLoaderByName('ModifiedConfig')) {
+           	$config->addLoader(new ConfigLoader($config->read('connector_root') . '/includes/configure.php'));
+        }
+		
+		// read db params from config
+        $dbconfig = $config->read("db");
+        
+		// get db singleton and connect
+        $db = Mysql::getInstance();        
+        
+        if (!$db->isConnected()) {
+            $db->connect(array(
+            	"host" => $dbconfig["host"], 
+            	"user" => $dbconfig["user"], 
+            	"password" => $dbconfig["pass"], 
+            	"name" => $dbconfig["name"]
+			));
+        }		      
+              
+        $db->setNames();
+		
+		// read modified in-shop configuration from db
+		$shopConfig = $db->query("SElECT configuration_key,configuration_value FROM configuration");
+		
+		foreach($shopConfig as $entry) {
+			$configArray[$entry['configuration_key']] = $entry['configuration_value'] == 'true' ? 1 : ($entry['configuration_value'] == 'false' ? 0 : $entry['configuration_value']);
+		}
+		
+		$configArray['img'] = $config->read("img");
+		$configArray['shop'] = $config->read("shop");
+		$configArray['shop']['fullUrl'] = $configArray['shop']['url'].$configArray['shop']['folder'];
+				
+		$session->shopConfig = $configArray;
+    }
+	
+    public function canHandle()
+    {
+        $controller = RpcMethod::buildController($this->getMethod()->getController());
+        $class = "\\jtl\\Connector\\Modified\\Controller\\{$controller}";
+        
+        if (class_exists($class)) {       
+            $this->_controller = $class::getInstance();
+            $this->_action = RpcMethod::buildAction($this->getMethod()->getAction());
+            
+            return is_callable(array($this->_controller, $this->_action));
+        }
+        
+        return false;
+    }
+
+    public function handle(RequestPacket $requestpacket)
+    {
+       	$this->init();
+
+       	$this->_controller->setMethod($this->getMethod());
+
+        // transaction        
+        if (($this->_action !== "commit")) {
+        	return $this->_controller->{$this->_action}($requestpacket->getParams());
+        }
+        else if (TransactionHandler::exists($requestpacket) && MainContainer::isMain($this->getMethod()->getController()) && $this->_action === "commit") {
+        	return $this->_controller->{$this->_action}($requestpacket->getParams(), $requestpacket->getGlobals()->getTransaction()->getId());
+        }
+        else {
+        	throw new TransactionException("Only Main Controller can handle commit actions");
+        }
+    }
+    
+    function error_handler($errno, $errstr, $errfile, $errline, $errcontext)
+    {
+        $types = array(
+            E_ERROR => 'E_ERROR',
+            E_WARNING => 'E_WARNING',
+            E_PARSE => 'E_PARSE',
+            E_NOTICE => 'E_NOTICE',
+            E_CORE_ERROR => 'E_CORE_ERROR',
+            E_CORE_WARNING => 'E_CORE_WARNING',
+            E_CORE_ERROR => 'E_COMPILE_ERROR',
+            E_CORE_WARNING => 'E_COMPILE_WARNING',
+            E_USER_ERROR => 'E_USER_ERROR',
+            E_USER_WARNING => 'E_USER_WARNING',
+            E_USER_NOTICE => 'E_USER_NOTICE',
+            E_STRICT => 'E_STRICT',
+            E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+            E_DEPRECATED => 'E_DEPRECATED',
+            E_USER_DEPRECATED => 'E_USER_DEPRECATED'
+        );
+    
+        file_put_contents("/tmp/error.log", date("[Y-m-d H:i:s] ") . "(" . $types[$errno] . ") File ({$errfile}, {$errline}): {$errstr}\n", FILE_APPEND);
+    }
+        
+    public function shutdown_handler()
+    {
+        if (($err = error_get_last())) {
+            ob_clean();
+
+            $error = new Error();
+            $error->setCode($err['type'])
+            ->setData('Shutdown! File: ' . $err['file'] . ' - Line: ' . $err['line'])
+            ->setMessage($err['message']);
+    
+            $reponsepacket = new ResponsePacket();
+            $reponsepacket->setError($error)
+            ->setJtlrpc("2.0");
+    
+            Response::send($reponsepacket);
+        }
+    }
+}
+?>
