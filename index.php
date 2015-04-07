c6e302ce870f2913a91f33dd147c9c8cc44533ae
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2015-04-07 14:16:59 +0200

    added exception handler before initialize

--- index.php
+++ index.php
@@ -6,9 +6,13 @@ use \jtl\Connector\Modified\Modified;
 
 defined('CONNECTOR_DIR') || define("CONNECTOR_DIR", __DIR__);
 
-if (!strpos($_SERVER['REQUEST_URI'], 'install')) {
-    $connector = Modified::getInstance();
-    $application = Application::getInstance();
-    $application->register($connector);
-    $application->run();
+try {
+    if (!strpos($_SERVER['REQUEST_URI'], 'install')) {
+        $connector = Modified::getInstance();
+        $application = Application::getInstance();
+        $application->register($connector);
+        $application->run();
+    }
+} catch (\Exception $exc) {
+    $connector->exceptionHandler($exc);
 }
