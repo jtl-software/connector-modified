376645067d495244d588af208a4eeaa2e29f1e9a
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2015-08-18 08:46:17 +0200

    refactored baseprice and units

--- src/jtl/Connector/Modified/Mapper/Product.php
+++ src/jtl/Connector/Modified/Mapper/Product.php
@@ -118,23 +118,37 @@ class Product extends BaseMapper
 
     protected function products_vpe($data)
     {
-        foreach ($data->getI18ns() as $i18n) {
-            $name = $i18n->getUnitName();
+        $name = $data->getBasePriceUnitName();
 
-            if (!empty($name)) {
+        if (!empty($name)) {
+            foreach ($data->getI18ns() as $i18n) {
                 $language_id = $this->locale2id($i18n->getLanguageISO());
-                $dbResult = $this->db->query('SELECT code FROM languages WHERE languages_id='.$language_id);
+                $dbResult = $this->db->query('SELECT code FROM languages WHERE languages_id=' . $language_id);
 
                 if ($dbResult[0]['code'] == $this->shopConfig['settings']['DEFAULT_LANGUAGE']) {
-                    $sql = $this->db->query('SELECT products_vpe_id FROM products_vpe WHERE language_id='.$language_id.' && products_vpe_name="'.$name.'"');
+                    $sql = $this->db->query('SELECT products_vpe_id FROM products_vpe WHERE language_id=' . $language_id . ' && products_vpe_name="' . $name . '"');
                     if (count($sql) > 0) {
                         return $sql[0]['products_vpe_id'];
+                    } else {
+                        $nextId = $this->db->query('SELECT max(products_vpe_id) + 1 AS nextID FROM products_vpe');
+                        $id = is_null($nextId[0]['nextID']) || $nextId[0]['nextID'] === 0 ? 1 : $nextId[0]['nextID'];
+
+                        foreach ($data->getI18ns() as $i18n) {
+                            $status = new \stdClass();
+                            $status->products_vpe_id = $id;
+                            $status->language_id = $this->locale2id($i18n->getLanguageISO());
+                            $status->products_vpe_name = $data->getBasePriceQuantity().' '.$name;
+
+                            $this->db->deleteInsertRow($status, 'products_vpe', array('products_vpe_id', 'language_id'), array($status->product_vpe_id, $status->language_id));
+                        }
+
+                        return $id;
                     }
                 }
             }
         }
 
-        return '';
+        return 0;
     }
 
     protected function products_shippingtime($data)
