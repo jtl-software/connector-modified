8e7befd24a192391c05a988277c75e99a1350956
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2018-10-18 10:16:10 +0200

    CO-323 #resolved Exchanged version file with build-config.yaml Fixed corrosponding changes in index, composer and build files

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 defined('CONNECTOR_DIR') || define("CONNECTOR_DIR", __DIR__);
-defined("CONNECTOR_VERSION") || define("CONNECTOR_VERSION", file_get_contents(__DIR__.'/version'));
 
 $loader = require_once __DIR__."/vendor/autoload.php";
 $loader->add('', CONNECTOR_DIR . '/plugins');
+
+$config = \Symfony\Component\Yaml\Yaml::parseFile(__DIR__ . '/build-config.yaml');
+defined('CONNECTOR_VERSION') || define('CONNECTOR_VERSION', $config['version']);
+
+
 
 use \jtl\Connector\Application\Application;
 use \jtl\Connector\Modified\Modified;
