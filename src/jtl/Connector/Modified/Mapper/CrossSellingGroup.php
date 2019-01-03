3b7ef815427aeece4719b95b86976971408d9159
Niklas Kokoschka <niklas.kokoschka@jtl-software.com>
2019-01-03 14:19:24 +0100

    CO-332 #resolved Crosssellgroups will now be mapped correctly.

--- src/jtl/Connector/Modified/Mapper/CrossSellingGroup.php
+++ src/jtl/Connector/Modified/Mapper/CrossSellingGroup.php
@@ -10,10 +10,48 @@ class CrossSellingGroup extends BaseMapper
         "getMethod" => "getCrossSellingGroups",
         "mapPull" => array(
             "id" => "products_xsell_grp_name_id",
-            "i18ns" => "CrossSellingGroupI18n|addI18n"
+            "i18ns" => "CrossSellingGroupI18n|addI18n",
         ),
-        "mapPush" => array(
-            "CrossSellingGroupI18n|addI18n" => "i18ns"
-        )
     );
+    
+    public function push($data, $dbObj = null)
+    {
+        $result = [];
+        $ids = [];
+        foreach ($data->getCrossSellingGroups() as $group) {
+            $id = $group->getId()->getEndpoint();
+            if (empty($id)) {
+                $nextId = $this->db->query('SELECT max(products_xsell_grp_name_id) + 1 AS nextID FROM products_xsell_grp_name');
+                $id = is_null($nextId[0]['nextID']) || $nextId[0]['nextID'] === 0 ? 1 : $nextId[0]['nextID'];
+            } else {
+                $this->db->query('DELETE FROM products_xsell_grp_name WHERE products_xsell_grp_name_id='.$id);
+            }
+            $group->getId()->setEndpoint($id);
+    
+            /** @var \jtl\Connector\Model\CrossSellingGroupI18n $i18n */
+            foreach ($group->getI18ns() as $i18n) {
+                $i18n->setCrossSellingGroupId($group->getId());
+                $i18ns[] = (new CrossSellingGroupI18n)->push($i18n);
+            }
+            $group->setI18ns($i18ns);
+            
+            $result[] = $group;
+            $ids[] = $group->getId()->getEndpoint();
+        }
+        
+        $ids = implode(",", $ids);
+        
+        $this->db->query(sprintf(" DELETE FROM products_xsell WHERE products_xsell_grp_name_id NOT IN (%s)",
+            $ids
+        ));
+    
+        $this->db->query(sprintf(" DELETE FROM products_xsell_grp_name WHERE products_xsell_grp_name_id NOT IN (%s)",
+            $ids
+        ));
+        
+          $this->db->query(sprintf(" DELETE FROM jtl_connector_link_crossselling_group WHERE endpoint_id NOT IN (%s)",
+            $ids
+        ));
+        return $result;
+    }
 }
