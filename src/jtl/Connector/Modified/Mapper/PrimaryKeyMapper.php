1f7ebdb45e6cfc9d9858e0f07c261e3cbc1b5e5c
Jan Weskamp <jan.weskamp@jtl-software.com>
2019-03-22 10:02:06 +0100

    Change to core identity constants

--- src/jtl/Connector/Modified/Mapper/PrimaryKeyMapper.php
+++ src/jtl/Connector/Modified/Mapper/PrimaryKeyMapper.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace jtl\Connector\Modified\Mapper;
 
 use jtl\Connector\Linker\IdentityLinker;
@@ -9,93 +10,97 @@ use jtl\Connector\Core\Logger\Logger;
 class PrimaryKeyMapper implements IPrimaryKeyMapper
 {
     protected $db;
-
-    protected static $types = array(
-        1 => 'category',
-        2 => 'customer',
-        4 => 'customer_order',
-        8 => 'delivery_note',
-        16 => 'image',
-        32 => 'manufacturer',
-        64 => 'product',
-        512 => 'payment',
-        1024 => 'crossselling',
-        2048 => 'crossselling_group'
-    );
-
+    
+    protected static $types = [
+        IdentityLinker::TYPE_CATEGORY           => 'category',
+        IdentityLinker::TYPE_CUSTOMER           => 'customer',
+        IdentityLinker::TYPE_CUSTOMER_ORDER     => 'customer_order',
+        IdentityLinker::TYPE_DELIVERY_NOTE      => 'delivery_note',
+        IdentityLinker::TYPE_IMAGE              => 'image',
+        IdentityLinker::TYPE_MANUFACTURER       => 'manufacturer',
+        IdentityLinker::TYPE_PRODUCT            => 'product',
+        IdentityLinker::TYPE_PAYMENT            => 'payment',
+        IdentityLinker::TYPE_CROSSSELLING       => 'crossselling',
+        IdentityLinker::TYPE_CROSSSELLING_GROUP => 'crossselling_group',
+    ];
+    
     public function __construct()
     {
         $this->db = Mysql::getInstance();
     }
-
+    
     public function getHostId($endpointId, $type)
     {
         if (isset(static::$types[$type])) {
             
             $dbResult = $this->db->query("SELECT host_id FROM jtl_connector_link_" . static::$types[$type] . " WHERE endpoint_id = '" . $endpointId . "'");
-    
+            
             $host_id = (count($dbResult) > 0) ? $dbResult[0]['host_id'] : null;
-        
-            Logger::write(sprintf('Trying to get host_id with endpoint_id (%s) and type (%s) ... host_id: (%s)', $endpointId, $type, $host_id), Logger::DEBUG, 'linker');
-        
+            
+            Logger::write(sprintf('Trying to get host_id with endpoint_id (%s) and type (%s) ... host_id: (%s)',
+                $endpointId, $type, $host_id), Logger::DEBUG, 'linker');
+            
             return $host_id;
         }
     }
-
+    
     public function getEndpointId($hostId, $type, $relationType = null)
     {
         if (isset(static::$types[$type])) {
-            $dbResult = $this->db->query("SELECT endpoint_id FROM jtl_connector_link_".static::$types[$type]." WHERE host_id = ".$hostId);
-    
+            $dbResult = $this->db->query("SELECT endpoint_id FROM jtl_connector_link_" . static::$types[$type] . " WHERE host_id = " . $hostId);
+            
             $endpoint_id = (count($dbResult) > 0) ? $dbResult[0]['endpoint_id'] : null;
-    
-            Logger::write(sprintf('Trying to get endpoint_id with host_id (%s) and type (%s) ... endpoint_id: (%s)', $hostId, $type, $endpoint_id), Logger::DEBUG, 'linker');
-    
+            
+            Logger::write(sprintf('Trying to get endpoint_id with host_id (%s) and type (%s) ... endpoint_id: (%s)',
+                $hostId, $type, $endpoint_id), Logger::DEBUG, 'linker');
+            
             return $endpoint_id;
         }
     }
-
+    
     public function save($endpointId, $hostId, $type)
     {
         if (isset(static::$types[$type])) {
-            Logger::write(sprintf('Save link with endpoint_id (%s), host_id (%s) and type (%s)', $endpointId, $hostId, $type), Logger::DEBUG, 'linker');
-    
-            $this->db->query("INSERT IGNORE INTO jtl_connector_link_".static::$types[$type]." (endpoint_id, host_id) VALUES ('".$endpointId."',".$hostId.")");
+            Logger::write(sprintf('Save link with endpoint_id (%s), host_id (%s) and type (%s)', $endpointId, $hostId,
+                $type), Logger::DEBUG, 'linker');
+            
+            $this->db->query("INSERT IGNORE INTO jtl_connector_link_" . static::$types[$type] . " (endpoint_id, host_id) VALUES ('" . $endpointId . "'," . $hostId . ")");
         }
     }
-
+    
     public function delete($endpointId = null, $hostId = null, $type)
     {
         if (isset(static::$types[$type])) {
-            Logger::write(sprintf('Delete link with endpoint_id (%s), host_id (%s) and type (%s)', $endpointId, $hostId, $type), Logger::DEBUG, 'linker');
-    
+            Logger::write(sprintf('Delete link with endpoint_id (%s), host_id (%s) and type (%s)', $endpointId, $hostId,
+                $type), Logger::DEBUG, 'linker');
+            
             $where = '';
-    
+            
             if ($endpointId && $endpointId != '') {
-                $where = 'endpoint_id = "'.$endpointId.'"';
+                $where = 'endpoint_id = "' . $endpointId . '"';
             }
-    
+            
             if ($hostId) {
-                $where = 'host_id = '.$hostId;
+                $where = 'host_id = ' . $hostId;
             }
-    
+            
             if (!empty($where)) {
                 $this->db->query('DELETE FROM jtl_connector_link_' . static::$types[$type] . ' WHERE ' . $where);
             }
         }
     }
-
+    
     public function clear()
     {
         Logger::write('Clearing linking tables', Logger::DEBUG, 'linker');
-
-        foreach(static::$types as $id => $name) {
-            $this->db->query('TRUNCATE TABLE jtl_connector_link_'.$name);
+        
+        foreach (static::$types as $id => $name) {
+            $this->db->query('TRUNCATE TABLE jtl_connector_link_' . $name);
         }
-
+        
         return true;
     }
-
+    
     public function gc()
     {
         return true;
